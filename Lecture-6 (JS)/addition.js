ba8e19b708e0db5baf2879afc3bade1c8bf4ec46
Mian Haseeb Nasir <mianhaseebnasir1@gmail.com>
2026-03-14 05:04:18 +0500

    Add files via upload

---
+++ Lecture-6 (JS)/addition.js
@@ -0,0 +1,9 @@
+// Operation (+)
+let a = Number(prompt("Choose a first number"));
+ b = Number(prompt("Choose a second number")); 
+c = a + b;
+alert(c);
+console.log(c);
+document.writeln("Addition is " + c);
+
+
